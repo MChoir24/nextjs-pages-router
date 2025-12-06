c9160016f4580d203d811bafd6fdc72da83d39b0
mchoir24 <miftahulkhoir9@gmail.com>
2025-12-06 21:08:19 +0700

    Update product detail page to display loading state and product name

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -9,13 +9,18 @@ export default function DetailProductPage() {
     `/api/products/${query.slug}`,
     fetcher
   );
+  !isLoading && data.data && console.log(data.data);
 
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-4">Product Detail Page</h1>
       <p>
         This is the detail page for{" "}
-        <span className="uppercase font-bold">{query.slug}</span>
+        {isLoading ? (
+          <span className="animate-pulse bg-gray-300 h-4 w-1/2 inline-block"></span>
+        ) : (
+          <span className="uppercase font-bold">{data?.data.name}</span>
+        )}
       </p>
     </div>
   );
