a9b48e7e9979b5d50fc169e9dc9980f8fd1923c2
mchoir24 <miftahulkhoir9@gmail.com>
2025-12-09 20:59:09 +0700

    feat: Add revalidation API and static products page

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  revalidated: boolean;
+  message?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (req.query.token !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({ revalidated: false, message: "Invalid token" });
+  }
+  try {// Add logic to determine which pages to revalidate
+    switch (req.query.data) { // Add more cases as needed
+      case "products":
+        await res.revalidate("/products/static");
+        return res.json({ revalidated: true, message: "Revalidation successful" });
+      default:
+        return res.status(400).json({ revalidated: false, message: "Invalid data parameter" });
+    }
+  } catch (error) {
+    return res.status(500).json({ revalidated: false, message: "Error revalidating" });
+  }
+}
---
+++ src/pages/products/static.tsx
@@ -0,0 +1,22 @@
+import ProductsView from "@/views/Products";
+import { Product } from "../api/[[...products]]";
+
+export default function ProductsPage({ products }: { products: Product[] }) {
+  return (
+    <>
+      <ProductsView products={products} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  // Fetch data from an API or database
+  const res = await fetch("http://localhost:3000/api/products");
+  const data = await res.json();
+  return {
+    props: {
+      products: data.data,
+    },
+    // revalidate: 10, // Revalidate every 60 seconds
+  };
+}
