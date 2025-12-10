e6971740bf21e1d9d3ff6999fab50e508451d102
Your Name <miftahulkhoir9@gmail.com>
2025-12-10 16:04:03 +0700

    feat: Add NextAuth authentication options with credentials provider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,49 @@
+import { NextAuthOptions } from "next-auth";
+import CreadentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CreadentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const hardCodedUser = {
+          id: "1",
+          name: "Admin User",
+          email: "admin@example.com",
+          password: "admin",
+        };
+        if (email === hardCodedUser.email && password === "admin") {
+          return hardCodedUser;
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, account, profile, user }) {
+      if (account?.provider === "credentials") {
+        token.email = user?.email;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if ("email" in token && session.user) {
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+};
