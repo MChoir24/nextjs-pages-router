3c76d7ffea0f08b55877bee2b2012758ce14777e
Your Name <miftahulkhoir9@gmail.com>
2026-01-22 15:56:57 +0700

    feat: Update authentication middleware to require access for both profile and products routes

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -1,14 +1,25 @@
 import { getToken } from "next-auth/jwt";
-import { NextFetchEvent, NextProxy, NextRequest, NextResponse } from "next/server";
+import {
+  NextFetchEvent,
+  NextProxy,
+  NextRequest,
+  NextResponse,
+} from "next/server";
 
-export default function withAuth(middleware: NextProxy, requireAuth: string[] = []) {
+export default function withAuth(
+  middleware: NextProxy,
+  requireAuth: string[] = [],
+) {
   return async (req: NextRequest, next: NextFetchEvent) => {
     const pathname = req.nextUrl.pathname;
     if (requireAuth.includes(pathname)) {
-      const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+      const token = await getToken({
+        req,
+        secret: process.env.NEXTAUTH_SECRET,
+      });
       if (!token) {
-        const loginUrl = new URL('/api/auth/signin', req.url);
-        loginUrl.searchParams.set('callbackUrl', req.nextUrl.href);
+        const loginUrl = new URL("/auth/login", req.url);
+        loginUrl.searchParams.set("callbackUrl", req.nextUrl.href);
         return NextResponse.redirect(loginUrl);
       }
     }
